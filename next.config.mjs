9750c942cfb894dc6ebd877d7d72b9b128a4d905
kaoutarel <kaoutar.elazzab@gmail.com>
2025-06-22 01:56:29 -0400

    Updated config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true, // ✅ disables blocking on ESLint during builds
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
